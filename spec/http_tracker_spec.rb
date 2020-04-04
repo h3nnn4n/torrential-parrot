386d4f28666922636000a53582459745a3074c22
h3nnn4n <uber.renan@gmail.com>
2020-04-04 16:51:28 -0300

    Test for http trackers

--- spec/http_tracker_spec.rb
+++ spec/http_tracker_spec.rb
@@ -22,4 +22,26 @@ RSpec.describe HttpTracker do
       expect(tracker.n_done).to eq(20)
     end
   end
+
+  describe '#announce' do
+    it 'returns a list of peers on success' do
+      tracker_url = 'http://bttracker.debian.org:6969/announce'
+      tracker = described_class.new(tracker_url)
+
+      allow(tracker).to receive(:peer_id).and_return('-PC0001-200367928925')
+
+      VCR.use_cassette 'http_tracker/announce_debian' do
+        expect(tracker.announce(torrent_debian)).to include(['180.150.6.209', 56_734])
+      end
+    end
+
+    it 'returns false on failure' do
+      tracker_url = 'http://bttracker.debian.org:6969/announce'
+      tracker = described_class.new(tracker_url)
+
+      allow(HTTParty).to receive(:get).and_raise(Net::ReadTimeout)
+
+      expect(tracker.announce(torrent_debian)).to be(false)
+    end
+  end
 end
