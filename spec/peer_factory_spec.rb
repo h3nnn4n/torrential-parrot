349c551140ddf26841147ec7068f0b47410011c9
h3nnn4n <uber.renan@gmail.com>
2020-04-04 16:24:22 -0300

    Stub udp calls

--- spec/peer_factory_spec.rb
+++ spec/peer_factory_spec.rb
@@ -6,21 +6,24 @@ require 'peer_factory'
 RSpec.describe PeerFactory do
   def trackers
     factory = TrackerFactory.new(torrent2)
-    factory.build
+    tracker_list = factory.build
+
+    tracker_list.each do |tracker|
+      allow(tracker).to receive(:connect).and_return(true)
+      allow(tracker).to receive(:announce).and_return([['186.232.38.137', 6_881]])
+    end
+
+    tracker_list
   end
 
   describe '#build' do
     it 'returns a list with all Peers' do
-      skip('Until I find a way to stub udp calls')
-
       factory = described_class.new(trackers, torrent2)
 
       expect(factory.build.size).to eq(1)
     end
 
     it 'returns a list of Peers' do
-      skip('Until I find a way to stub udp calls')
-
       factory = described_class.new(trackers, torrent2)
 
       expect(factory.build).to all(be_a(Peer))
