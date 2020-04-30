aeb5b28d974cd90ee7db026da992c50256b7cf1d
h3nnn4n <uber.renan@gmail.com>
2020-04-30 09:14:53 -0300

    Read config from a config yml file and fallback to defaults if not available

--- lib/config.rb
+++ lib/config.rb
@@ -1,43 +1,58 @@
 # frozen_string_literal: true
 
+require 'yaml'
+
 class Config
+  CONFIG_PATH = './config.yml'
+
   # How many piece requests we can have pending at once, per peer
   def self.max_peer_requests
-    5
+    config_from_file[__method__.to_s] || 5
   end
 
   # How log we wait before considering a request as failed and retrying
   def self.peer_request_timeout
-    10
+    config_from_file[__method__.to_s] || 10
   end
 
   # This is how long it takes for a chunk request to be timedout If this value
   # is less than peer_request_timeout, then the same chunk will get
   # re-requested from another peer before the original peer times out
   def self.chunk_request_timeout
-    5
+    config_from_file[__method__.to_s] || 5
   end
 
   def self.chunk_size
-    16_384
+    config_from_file[__method__.to_s] || 16_384
   end
 
   def self.main_loop_sleep_amount
-    0.5
+    config_from_file[__method__.to_s] || 0.5
   end
 
   # How many peers should we be connecting at once
   def self.max_peer_connetions
-    8
+    config_from_file[__method__.to_s] || 8
   end
 
   # Socket read timeout for peer the peer protocol
   def self.peer_read_timeout
-    1.0
+    config_from_file[__method__.to_s] || 1.0
   end
 
   # Block size when reading from sockets
   def self.block_read_size
-    1_024
+    config_from_file[__method__.to_s] || 1_024
+  end
+
+  def self.config_from_file
+    @config_from_file ||= begin
+      if File.file?(CONFIG_PATH)
+        data = File.read(CONFIG_PATH)
+        YAML.safe_load(data)
+      else
+        {}
+      end
+    end
   end
 end
